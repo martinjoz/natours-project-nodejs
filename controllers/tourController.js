7c456d7a379fc4562fbcddece77249f7e8177d0d
mac <martinnjoroge0028@gmail.com>
2024-05-03 14:58:54 +0300

    Added 404 error

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,3 +1,4 @@
+const AppError = require('../utils/appError');
 const Tour = require('./../models/tourModel');
 
 //Error handling for all async function
@@ -9,6 +10,7 @@ const catchAsync = require('./../utils/catchAsync');
 
 exports.getTours = catchAsync(async (req, res, next) => {
   const tours = await Tour.find();
+
   res.status(200).json({
     status: 'success',
     dataLength: tours.length,
@@ -19,6 +21,11 @@ exports.getTours = catchAsync(async (req, res, next) => {
 
 exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
+
+  if (!tour) {
+    return next(new AppError('No tour found with that ID', 404));
+  }
+
   res.status(200).json({
     status: 'Success',
     data: { tour },
