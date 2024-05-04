7dbcc2450248cb176efa9f33f568562e88525a4e
mac <martinnjoroge0028@gmail.com>
2024-05-04 13:46:53 +0300

    Added bcrypt

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
-const mongoose = required('mongoose');
-const validator = required('validator');
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -28,7 +29,26 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password'],
+    validate: {
+      validator: function (val) {
+        return val === this.password;
+      },
+      message: 'Passwords entered should match.',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  //check if the password field is modified
+  if (!this.isModified('password')) return next();
+
+  //Hash the password
+  this.password = await bcrypt.hash(this.password, 12);
+
+  //Delete password confirm
+  this.passwordConfirm = undefined;
+
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,30 +1,35 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
 
-//Controller for Users
-const getUsers = (req, res) => {
-    res.status(500).json({ status: 'error', message: 'Route undefined' })
-}
-const getUser = (req, res) => {
-    res.status(500).json({ status: 'error', message: 'Route undefined' })
-}
-const updateUser = (req, res) => {
-    res.status(500).json({ status: 'error', message: 'Route undefined' })
-}
-const createUser = (req, res) => {
-    res.status(500).json({ status: 'error', message: 'Route undefined' })
-}
-const deleteUser = (req, res) => {
-    res.status(500).json({ status: 'error', message: 'Route undefined' })
-}
+const auth = require('./../controllers/auth/auth');
 
-router.route('/') //This will be treated as /api/v1/users
-    .get(getUsers)
-    .post(createUser);
+//Auth routes
+router.post('/signup', auth.signup);
 
-router.route('/:id')
-    .get(getUser)
-    .patch(updateUser)
-    .delete(deleteUser);
+// //Controller for Users
+// const getUsers = (req, res) => {
+//     res.status(500).json({ status: 'error', message: 'Route undefined' })
+// }
+// const getUser = (req, res) => {
+//     res.status(500).json({ status: 'error', message: 'Route undefined' })
+// }
+// const updateUser = (req, res) => {
+//     res.status(500).json({ status: 'error', message: 'Route undefined' })
+// }
+// const createUser = (req, res) => {
+//     res.status(500).json({ status: 'error', message: 'Route undefined' })
+// }
+// const deleteUser = (req, res) => {
+//     res.status(500).json({ status: 'error', message: 'Route undefined' })
+// }
+
+// router.route('/') //This will be treated as /api/v1/users
+//     .get(getUsers)
+//     .post(createUser);
+
+// router.route('/:id')
+//     .get(getUser)
+//     .patch(updateUser)
+//     .delete(deleteUser);
 
 module.exports = router;
